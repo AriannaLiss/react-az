2c26c7166883094189c2b985b4ccde068c81a441
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-05-19 10:47:18 +0200

    Dynamic redirect. Page of posts + comments. useNavigate, useParams.

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MyButton from './UI/button/MyButton';
+import { useNavigate } from "react-router-dom";
 
 const PostItem = ({post, id, remove}) => {
+    const router = useNavigate();
     return (
         <div className = "post">
             <div className='post__content'>
@@ -11,6 +13,7 @@ const PostItem = ({post, id, remove}) => {
                 </div>
             </div>
             <div className = 'post__btns'>
+                <MyButton onClick={()=>router( `/posts/${id}`)}>Open</MyButton>
                 <MyButton onClick={()=>remove(post)}>Delete</MyButton>
             </div>
         </div>
